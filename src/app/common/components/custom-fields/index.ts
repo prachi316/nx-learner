4a23837f4ae37c549629a9b55bc358222eada2fd
Prachi <prachi.sp316@gmail.com>
2025-10-08 18:14:03 +0530

    Refactor SelectInput export to separate type definition for improved clarity

--- src/app/common/components/custom-fields/index.ts
+++ src/app/common/components/custom-fields/index.ts
@@ -3,4 +3,5 @@ export { DateInputComponent } from './date-input/date-input.component';
 export { TimeInputComponent } from './time-input/time-input.component';
 export { TextInputComponent } from './text-input/text-input.component';
 export { TextareaInputComponent } from './textarea-input/textarea-input.component';
-export { SelectInputComponent, SelectOption } from './select-input/select-input.component';
+export { SelectInputComponent } from './select-input/select-input.component';
+export type { SelectOption } from './select-input/select-input.component';
